8bc99d726d2c26b0a05fd573ff478916ecb9ccd9
Love <panter9805@gmail.com>
2017-03-02 21:54:52 +0300

    initial commit

---
+++ Database/Tables/Realty.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Realty]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Title] NVARCHAR(50) NOT NULL, 
+    [Address] NVARCHAR(100) NOT NULL, 
+    [Price] MONEY NOT NULL
+)
---
+++ realtyApp/RealEstate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealtyApp
+{
+    /// <summary>
+    /// Объект недвижимости
+    /// </summary>
+    public class RealEstate
+    {
+        private string _title;
+        private string _address;
+        private decimal _price;
+
+        /// <summary>
+        /// Название
+        /// </summary>
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        /// <summary>
+        /// Адрес
+        /// </summary>
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        /// <summary>
+        /// Цена
+        /// </summary>
+        public decimal Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+    }
+}
