2157ff572c20521dd65d23d276d8396529c1a287
Love <panter9805@gmail.com>
2017-05-08 21:47:25 +0300

    Owner & RealEstate window

---
+++ realtyApp/OwnerWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using RealtyApp.Models;
+
+namespace RealtyApp
+{
+    /// <summary>
+    /// Interaction logic for NewOwnerWindow.xaml
+    /// </summary>
+    public partial class OwnerWindow : Window
+    {
+        Owner _owner;
+
+        public OwnerWindow()
+        {
+            InitializeComponent();
+
+            _labelError.Visibility = Visibility.Hidden;
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(_textBoxFullName.Text))
+            {
+                _labelError.Visibility = Visibility.Visible;
+                _labelError.Content = "Введите фамилию владельца недвижимости";
+                _textBoxFullName.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(_textBoxPhoneNumber.Text))
+            {
+                _labelError.Visibility = Visibility.Visible;
+                _labelError.Content = "Введите мобильный телефон владельца недвижимости";
+                _textBoxPhoneNumber.Focus();
+                return;
+            }
+
+
+            _owner = new Owner
+            {
+                FullName = _textBoxFullName.Text,
+                PhoneNumber = _textBoxPhoneNumber.Text
+            };
+            
+            DialogResult = true;
+        }
+    }
+}
---
+++ realtyApp/RealEstateWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using RealtyApp.Models;
+
+namespace RealtyApp
+{
+    /// <summary>
+    /// Interaction logic for NewRealEstateWindow.xaml
+    /// </summary>
+    public partial class RealEstateWindow : Window
+    {      
+
+        public RealEstateWindow(List<Owner> owners)
+        {
+            InitializeComponent();
+
+            comboBoxFaculties.ItemsSource = owners;
+        }
+
+        RealEstate _newRealEstate;
+
+        public RealEstate NewRealEstate
+        {
+            get {
+                return _newRealEstate;
+            }
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(_textBoxTitle.Text))
+            {
+                _labelError.Visibility = Visibility.Visible;
+                _labelError.Content = "Введите название объекта недвижимости";
+                _textBoxTitle.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(_textBoxAddress.Text))
+            {
+                _labelError.Visibility = Visibility.Visible;
+                _labelError.Content = "Введите адрес объекта недвижимости";
+                _textBoxAddress.Focus();
+                return;
+            }
+
+            decimal price;
+
+            if (!decimal.TryParse(_textBoxPrice.Text, out price)) {
+                _labelError.Visibility = Visibility.Visible;
+                _labelError.Content = "Введите стоимость объекта недвижимости";
+                _textBoxPrice.Focus();
+                return;
+            }
+            if (price <= 100000) {
+                _labelError.Visibility = Visibility.Visible;
+                _labelError.Content = "Стоимость недвижимости от 100000 руб";
+                _textBoxPrice.Focus();
+                return;
+            }
+
+            if (comboBoxFaculties.SelectedItem == null) {
+                MessageBox.Show("Необходимо выбрать факультет");
+                comboBoxFaculties.Focus();
+                return;
+            }
+            
+            _newLecturer = new Lecturer(textBoxFio.Text,
+                rating);
+            _newLecturer.Faculty = comboBoxFaculties.SelectedItem as Faculty;
+            // Close current window
+            DialogResult = true;
+        }
+    }
+}
